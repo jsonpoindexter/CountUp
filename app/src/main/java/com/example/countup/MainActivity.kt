ac12c512aa958972a6cd684ddded815f41763777
Jason Poindexter <poindexter.json@gmail.com>
2022-01-19 09:51:54 -0800

    Style frame w/ header, and day values using static data

--- app/src/main/java/com/example/countup/MainActivity.kt
+++ app/src/main/java/com/example/countup/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.countup
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.countup.databinding.ActivityMainBinding
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 class MainActivity : Activity() {
 
@@ -13,6 +17,20 @@ class MainActivity : Activity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val typeView = findViewById<TextView>(R.id.type)
+        @SuppressLint("SetTextI18n") // TODO: remove once dynamic
+        typeView.text = "Hospital"
+
+        // Start date will be user input
+        val startDate = LocalDate.of(2021, 11, 28)
+        val currDate = LocalDate.now()
+        println(startDate)
+        println(currDate)
+        val diff = ChronoUnit.DAYS.between(startDate, currDate)
+        println("DateDiff: $diff")
+        val daysView = findViewById<TextView>(R.id.daysValue)
+        daysView.text = diff.toString()
 
     }
 }
